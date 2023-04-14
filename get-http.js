0e55e083ebe733828e238e26418e8c3a07356dc5
tothemoon6789 <115088780+tothemoon6789@users.noreply.github.com>
2023-04-14 08:45:41 +0700

    Update get-http.js

--- get-http.js
+++ get-http.js
@@ -1,6 +1,6 @@
 const httpArray = []
 document.querySelectorAll('.website_text').forEach((item)=>{
-    const http = item.firstChild.innerHTML;
+    const http = "http://"+item.firstChild.innerHTML+"/";
     httpArray.push(http)
     console.log(httpArray)
 })
